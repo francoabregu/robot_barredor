a5464c717708fbcc87bbdacd0e2297bb299c9ffd
Franco Abregu <francoabregu93@gmail.com>
2018-06-27 15:59:15 -0300

    agrego girar a la izquierda y a la derecha y ordeno código

--- robot_barredor.ino
+++ robot_barredor.ino
@@ -1,21 +1,25 @@
+#include <Servo.h>
+
+// Motores
 const int pinENA = 6;
 const int pinIN1 = 8;
 const int pinIN2 = 9;
 const int pinIN3 = 10;
 const int pinIN4 = 12;
 const int pinENB = 7;
-
-int distance;
-
-const int trigPin = 3;
-const int echoPin = 2;
- 
+const int pinMotorIzquierda[3] = { pinENA, pinIN1, pinIN2 }; // el primero es hacia adelante, el segundo hacia atras y el tercero la velocidad de giro
+const int pinMotorDerecha[3] = { pinENB, pinIN3, pinIN4 };// el primero es hacia adelante, el segundo hacia atras y el tercero la velocidad de giro
 const int waitTime = 1000;   //espera entre fases
 const int speed = 200;      //velocidad de giro
- 
-const int pinMotorA[3] = { pinENA, pinIN1, pinIN2 };
-const int pinMotorB[3] = { pinENB, pinIN3, pinIN4 };
- 
+
+// Sensor ultrasónico
+int distance;
+const int trigPin = 3;
+const int echoPin = 2;
+
+Servo myservo; // set myservo
+
+
 void setup()
 {
    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -27,33 +31,26 @@ void setup()
    pinMode(pinIN4, OUTPUT);
    pinMode(pinENB, OUTPUT);
    Serial.begin(9600);
+   myservo.attach (5); // Define the servo motor output pin 5 (PWM)
 }
  
 void loop()
 {
-  avanzar(speed);
+  avanzar(10);
   distance = medir();
     if(distance < 6){
-      fullStop(pinMotorA);
-      fullStop(pinMotorB);
+      detenerse(10);
     }
-   delay(waitTime);
-  moveForward(pinMotorA, speed);
-  moveForward(pinMotorB, speed);
-delay(waitTime);
- 
-   //fullStop(pinMotorA);
-   //fullStop(pinMotorB);
-   //delay(waitTime);
+    avanzar(10);
 }
  
-void moveForward(const int pinMotor[3], int speed){
+void moveForward(const int pinMotor[3]){
    digitalWrite(pinMotor[1], HIGH);
    digitalWrite(pinMotor[2], LOW);
    analogWrite(pinMotor[0], speed);
 }
  
-void moveBackward(const int pinMotor[3], int speed){
+void moveBackward(const int pinMotor[3]){
    digitalWrite(pinMotor[1], LOW);
    digitalWrite(pinMotor[2], HIGH);
    analogWrite(pinMotor[0], speed);
@@ -65,37 +62,67 @@ void fullStop(const int pinMotor[3]){
    analogWrite(pinMotor[0], 0);
 }
 
-void avanzar(int speed){
-  moveForward(pinMotorA, speed);
-  moveForward(pinMotorB, speed);
-  delay(waitTime);
+void avanzar(int valor){
+  moveForward(pinMotorIzquierda);
+  moveForward(pinMotorDerecha);
+  delay(valor * 100);
 }
 
-void retroceder(int speed){
-  moveBackward(pinMotorA, speed);
-  moveBackward(pinMotorB, speed);
+void detenerse(int valor){
+  fullStop(pinMotorIzquierda);
+  fullStop(pinMotorDerecha);
+  delay(valor * 10);
+}
+
+void retroceder(int valor){
+  moveBackward(pinMotorIzquierda);
+  moveBackward(pinMotorDerecha);
+  delay(valor * 100);
 }
 
 // girar a la derecha con una sola rueda suponiendo que motor B es el de la derecha y motor A el de la izquierda
-void derecha(int speed){
-      digitalWrite (pinMotorB[2], LOW);
-      digitalWrite (pinMotorB[1], HIGH);
-      digitalWrite (pinMotorA[2], HIGH);
-      digitalWrite (pinMotorA[1], HIGH);
-      analogWrite(pinMotorA[0],speed);
-      analogWrite(pinMotorB[0], speed);
-      delay(waitTime);
+void derecha(int valor){
+      digitalWrite(pinMotorDerecha[2], LOW);
+      digitalWrite(pinMotorDerecha[1], HIGH);
+      digitalWrite(pinMotorIzquierda[2], HIGH);
+      digitalWrite(pinMotorIzquierda[1], HIGH);
+      analogWrite(pinMotorIzquierda[0],speed);
+      analogWrite(pinMotorDerecha[0], speed);
+      delay(valor * 100);
      }
 // girar a la izquierda con una sola rueda suponiendo que motor B es el de la derecha y motor A el de la izquierda
-void izquierda(int speed){
-      digitalWrite (pinMotorB[2], HIGH);
-      digitalWrite (pinMotorB[1], HIGH);
-      digitalWrite (pinMotorA[2], LOW); // cause the motor (left rear) to operate
-      digitalWrite (pinMotorA[1], HIGH);
-      analogWrite(pinMotorA[0],speed);
-      analogWrite(pinMotorB[0], speed);
-      delay (waitTime);
+void izquierda(int valor){
+      digitalWrite(pinMotorDerecha[2], HIGH);
+      digitalWrite(pinMotorDerecha[1], HIGH);
+      digitalWrite(pinMotorIzquierda[2], LOW); // cause the motor(left rear) to operate
+      digitalWrite(pinMotorIzquierda[1], HIGH);
+      analogWrite(pinMotorIzquierda[0],speed);
+      analogWrite(pinMotorDerecha[0], speed);
+      delay(valor * 100);
 }
+
+// con las dos ruedas
+void girarALaDerecha(int valor){
+      digitalWrite(pinMotorDerecha[2], LOW); // cause the motor (right rear) to operate
+      digitalWrite(pinMotorDerecha[1], HIGH);
+      digitalWrite(pinMotorIzquierda[2], HIGH);
+      digitalWrite(pinMotorDerecha[1], LOW); // cause the motor (left front) to operate
+      analogWrite(pinMotorIzquierda[0],speed);
+      analogWrite(pinMotorDerecha[0], speed);
+      delay(valor * 100);
+
+}
+
+//con las dos ruedas
+void girarALaIzquierda(int valor){
+      digitalWrite(pinMotorDerecha[2], HIGH);
+      digitalWrite(pinMotorDerecha[1], LOW); // cause the motor (right front) to operate
+      digitalWrite(pinMotorIzquierda[2], LOW); // cause the motor (left rear) to operate
+      digitalWrite(pinMotorIzquierda[1], HIGH);
+      analogWrite(pinMotorIzquierda[0],speed);
+      analogWrite(pinMotorDerecha[0], speed);
+      delay(valor * 100);
+     }
 
 int medir(){
     long duracion;
@@ -110,5 +137,3 @@ int medir(){
     Serial.println(distancia);
     return distancia;
 }
-
-
